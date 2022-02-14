974796c7a97ecc26597ff8997374fd3e5fdf2995
Mahesa Rafian Syah <mahesarafiansyah@gmail.com>
2022-02-14 19:08:57 +0700

    Update style

--- regform.php
+++ regform.php
@@ -36,42 +36,9 @@ else {
     </head>
     <body>
         <!-- NAV -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+        <?php
+            include 'main-nav.php';
+        ?>
 
 
 
@@ -85,7 +52,7 @@ else {
              <a href="#" class="logo">MR <span>.</span></a>
  
              <div class="heading">
-                <h2>Marina</h2>
+                <h2>HYDRO</h2>
                 <p>Your Right Choice</p>
              </div>
  
@@ -102,12 +69,13 @@ else {
              <!-- Login Form -->
              <div class="login form-peice switched">
                 <form class="" action="#" method="post">
-                   <div class="form-group">
+                   <div class="form-group"  style="color: black;">
+                   <h3>Login</h3>
                       <label for="loginemail">Email Adderss</label>
                       <input type="email" name="loginemail" id="loginemail" required>
                    </div>
  
-                   <div class="form-group">
+                   <div class="form-group"  style="color: black;">
                       <label for="loginPassword">Password</label>
                       <input type="password" name="loginPassword" id="loginPassword" required>
                    </div>
@@ -124,8 +92,9 @@ else {
              <div class="signup form-peice">
                 <form class="" action="#" method="post">
  
-                   <div class="form-group">
-                      <label for="username">Username</label>
+                   <div class="form-group" style="color: black;">
+                     <h3>Sign Up</h3>
+                      <label for="username" >Username</label>
                       <input type="text" name="username" id="username" class="">
                       <span class="error"></span>
                    </div>
@@ -141,13 +110,13 @@ else {
                       <input type="text" name="phone" id="phone">
                    </div>-->
  
-                   <div class="form-group">
+                   <div class="form-group" style="color: black;">
                       <label for="password">Password</label>
                       <input type="password" name="password" id="password" class="">
                       <span class="error"></span>
                    </div>
  
-                   <div class="form-group">
+                   <div class="form-group" style="color: black;">
                       <label for="passwordCon">Confirm Password</label>
                       <input type="password" name="password2" id="password2" class="">
                       <span class="error"></span>
@@ -156,7 +125,7 @@ else {
                    <div class="CTA">
                    <input type="submit" name="register" id="register" value="Sign Up">
                    
-                      <a href="#" class="switch">I have an account</a>
+                      <a href="#" class="switch" style="color: black;">I have an account</a>
                    </div>
                 </form>
              </div><!-- End Signup Form -->
@@ -172,145 +141,11 @@ else {
  
 
     <!-- Footer -->
-    <footer
-            class="text-center text-lg-start text-dark"
-            style="background-color: #ECEFF1"
-            >
-      <!-- Section: Social media -->
-      <section
-               class="d-flex justify-content-between p-4 text-white"
-               style="background-color: #21D192"
-               >
-        <!-- Left -->
-        <div class="me-5">
-          <span>Get connected with us on social networks:</span>
-        </div>
-        <!-- Left -->
-  
-        <!-- Right -->
-        <div>
-          <a href="" class="text-white me-4">
-            <i class="fab fa-facebook-f"></i>
-          </a>
-          <a href="" class="text-white me-4">
-            <i class="fab fa-twitter"></i>
-          </a>
-          <a href="" class="text-white me-4">
-            <i class="fab fa-google"></i>
-          </a>
-          <a href="" class="text-white me-4">
-            <i class="fab fa-instagram"></i>
-          </a>
-          <a href="" class="text-white me-4">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="" class="text-white me-4">
-            <i class="fab fa-github"></i>
-          </a>
-        </div>
-        <!-- Right -->
-      </section>
-      <!-- Section: Social media -->
-  
-      <!-- Section: Links  -->
-      <section class="">
-        <div class="text-center text-md-start mt-5" style="padding-left: 20px; padding-right: 20px;">
-          <!-- Grid row -->
-          <div class="row mt-3">
-            <!-- Grid column -->
-            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              <!-- Content -->
-              <h6 class="text-uppercase fw-bold">Company name</h6>
-              <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
-                  />
-              <p>
-                Here you can use rows and columns to organize your footer
-                content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit.
-              </p>
-            </div>
-            <!-- Grid column -->
-  
-            <!-- Grid column -->
-            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-              <!-- Links -->
-              <h6 class="text-uppercase fw-bold">Products</h6>
-              <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
-                  />
-              <p>
-                <a href="#!" class="text-dark">MDBootstrap</a>
-              </p>
-              <p>
-                <a href="#!" class="text-dark">MDWordPress</a>
-              </p>
-              <p>
-                <a href="#!" class="text-dark">BrandFlow</a>
-              </p>
-              <p>
-                <a href="#!" class="text-dark">Bootstrap Angular</a>
-              </p>
-            </div>
-            <!-- Grid column -->
-  
-            <!-- Grid column -->
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-              <!-- Links -->
-              <h6 class="text-uppercase fw-bold">Useful links</h6>
-              <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
-                  />
-              <p>
-                <a href="#!" class="text-dark">Your Account</a>
-              </p>
-              <p>
-                <a href="#!" class="text-dark">Become an Affiliate</a>
-              </p>
-              <p>
-                <a href="#!" class="text-dark">Shipping Rates</a>
-              </p>
-              <p>
-                <a href="#!" class="text-dark">Help</a>
-              </p>
-            </div>
-            <!-- Grid column -->
-  
-            <!-- Grid column -->
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-              <!-- Links -->
-              <h6 class="text-uppercase fw-bold">Contact</h6>
-              <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
-                  />
-              <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-              <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-              <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-              <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-            </div>
-            <!-- Grid column -->
-          </div>
-          <!-- Grid row -->
-        </div>
-      </section>
-      <!-- Section: Links  -->
-  
-      <!-- Copyright -->
-      <div
-           class="text-center p-3"
-           style="background-color: rgba(0, 0, 0, 0.2)"
-           >
-        © 2020 Copyright:
-        <a class="text-dark" href="https://mdbootstrap.com/"
-           >MDBootstrap.com</a
-          >
-      </div>
-      <!-- Copyright -->
-    </footer>
+    <?php
+    
+    include 'main-footer.php';
+
+    ?>
     <!-- Footer -->
 
  
