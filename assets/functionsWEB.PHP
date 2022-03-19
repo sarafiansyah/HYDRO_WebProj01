fd506a8984d65d46b2e2bb12dab6aa54c551d195
Mahesa Rafian Syah <mahesarafiansyah@gmail.com>
2022-03-19 20:57:14 +0700

    Assets

---
+++ assets/functionsWEB.PHP
@@ -0,0 +1,39 @@
+<?php 
+
+// Create connection
+$conn = mysqli_connect("sql210.epizy.com", "epiz_31147817", "BOTOoxQNal5mGo5", "epiz_31147817_hydro_web");
+
+    function registration($data){
+    global $conn;
+
+    $username = strtolower(stripslashes($data["username"]));
+    $password = mysqli_real_escape_string($conn, $data["password"]);
+    $password2 = mysqli_real_escape_string($conn, $data["password2"]);
+
+    //Username Confirmation
+    $resultReg = mysqli_query($conn, "SELECT username FROM users WHERE 
+    username = '$username'");
+
+    if(mysqli_fetch_assoc($resultReg)){
+        echo "<script> alert ('sudah terdafdtar'); </script>";
+        return false;
+    }
+
+    //Password Confirmation
+    if($password !== $password2) {
+        echo "<script> alert ('salah'); </script>";
+        return false;
+    }
+
+    //Password encryption, PASSWORD_DEFAULT Method
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    
+
+    //User add 
+    mysqli_query($conn, "INSERT INTO users VALUES('', '$username', '$password')");
+
+    return mysqli_affected_rows($conn);
+
+    //return 1;
+}
+?>
