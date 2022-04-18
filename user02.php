b30ac8915c806d5c2fc2bec4b586c35ccb56f944
Mahesa Rafian Syah <mahesarafiansyah@gmail.com>
2022-04-18 16:11:57 +0700

    Update user02.php

--- user02.php
+++ user02.php
@@ -21,15 +21,28 @@ else {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Funda of Web IT</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+	<title>Profile Page</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/all.min.css" integrity="sha512-c93ifPoTvMdEJH/rKIcBx//AL1znq9+4/RmMGafI/vnTFe/dKwnn1uoeszE2zJBQTS1Ck5CqSBE+34ng2PthJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+
+
+
 </head>
 <body>
-    
-    <div class="container">
+<?php
+	include 'main-nav.php';
+?>
+    <div class="container mb-5">
         <div class="row justify-content-center">
             <div class="col-md-7">
 
@@ -42,10 +55,11 @@ else {
                         <form action="" method="GET">
                             <div class="row">
                                 <div class="col-md-8">
+                                <label for="">Search Username</label>
                                     <input type="text" name="stud_id" value="<?php if(isset($_GET['stud_id'])){echo $_GET['stud_id'];} ?>" class="form-control">
                                 </div>
                                 <div class="col-md-4">
-                                    <button type="submit" class="btn btn-primary">Search</button>
+                                    <button type="submit" class="btn btn-primary mt-4">Search</button>
                                     
                                 </div>
                             </div>
@@ -120,5 +134,14 @@ else {
 
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+
+ <!-- Footer -->
+ <?php
+    
+    include 'main-footer.php';
+
+    ?>
+    <!-- Footer -->
+
 </body>
 </html>
